f32483904328ba12ba9a1c92bc08991afa5eab17
Sajid Chowdhury <msrchk@gmail.com>
2021-02-14 23:08:35 +0600

    last push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-const main = document.querySelector('.main')
+const main = document.querySelector('.main');
 // selected image 
 let sliders = [];
 // If this key doesn't work
@@ -21,7 +21,7 @@ const showImages = (images) => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2 ';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
+    gallery.appendChild(div);
     toggleSpinner(false);
   })
 }
@@ -34,7 +34,6 @@ const getImages = (query) => {
     .catch(err => console.log(err))
 }
 let slideIndex = 0;
-let count = 0;
 const selectItem = (event, img) => {
   let element = event.target;
   let item = sliders.indexOf(img);
@@ -42,22 +41,17 @@ const selectItem = (event, img) => {
     element.classList.add('added');
     sliders.push(img);
   } else {
-    //alert('Hey, Already added !')
-    //sliders.pop(img);
-    sliders.splice(item, 1)
+    sliders.splice(item, 1);
     element.classList.remove('added');
-    count++;
-    showSliderItem(count);
   }
 }
-const showSliderItem = (length, value = count) => {
+const showSliderItem = length => {
   const time = document.getElementById('duration').value;
-  //document.getElementById("sliderTotal").innerText =`You selected total ${length} images in the slide show and deselected ${value} times. You put ${time} ms in duration field.`
-  if (time < 1000) {
-    document.getElementById("sliderTotal").innerText = `You selected total ${length} images in the slide show and deselected ${value} times. You put ${time} ms in duration field but its working on 1000 ms`
+  if (time < 0000) {
+    document.getElementById("sliderTotal").innerText = `You selected total ${length} images in the slide show. You put ${time} ms in duration input field but its working on 1000 ms.`
   }
   else {
-    document.getElementById("sliderTotal").innerText = `You selected total ${length} images in the slide show and deselected ${value} times. You put ${time} ms in duration field.`
+    document.getElementById("sliderTotal").innerText = `You selected total ${length} images in the slide show.You put ${time} ms in duration field.`
   }
 }
 var timer
@@ -81,7 +75,7 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   let duration = document.getElementById('duration').value;
-  if (duration > 1000) {
+  if (duration > 0000) {
     slideShow();
   }
   else {
@@ -96,14 +90,13 @@ const createSlider = () => {
 }
 const slideShow = () => {
   sliders.forEach(slide => {
-    let item = document.createElement('div')
+    let item = document.createElement('div');
     item.className = "slider-item";
     item.innerHTML = `<img class="w-100" onclick=removeSlider("${slide}") id="slider-item"
     src="${slide}"
     alt="">`;
     sliderContainer.appendChild(item);
   })
-
 }
 // change slider index 
 const changeItem = index => {
@@ -113,7 +106,7 @@ const changeItem = index => {
 const changeSlide = (index) => {
   const items = document.querySelectorAll('.slider-item');
   if (index < 0) {
-    slideIndex = items.length - 1
+    slideIndex = items.length - 1;
     index = slideIndex;
   };
   if (index >= items.length) {
@@ -121,30 +114,36 @@ const changeSlide = (index) => {
     slideIndex = 0;
   }
   items.forEach(item => {
-    item.style.display = "none"
+    item.style.display = "none";
   })
-  items[index].style.display = "block"
+  items[index].style.display = "block";
 }
 search.addEventListener("keypress", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.querySelector('.main').style.display = 'none';
-    clearInterval(timer);
-    const search = document.getElementById('search');
-    getImages(search.value)
-    sliders.length = 0;
+    btnOrEnterClick();
   }
 })
+searchBtn.addEventListener("click", function () {
+  btnOrEnterClick();
+})
+const btnOrEnterClick = () => {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
+}
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
 const toggleSpinner = (show) => {
   const spinner = document.getElementById("spinner");
   if (show) {
-    spinner.classList.remove('d-none')
+    spinner.classList.remove('d-none');
   }
   else {
-    spinner.classList.add('d-none')
+    spinner.classList.add('d-none');
   }
 }
 const userInfo = () => {

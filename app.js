fbfbe8ef533359c460b6128a657242f478ac2139
Sajid Chowdhury <msrchk@gmail.com>
2021-02-14 14:31:39 +0600

    added

---
+++ app.js
@@ -0,0 +1,167 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+// selected image 
+let sliders = [];
+
+// If this key doesn't work
+// Find the name in the url and go to their website
+// to create your own api key
+const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+
+// show images 
+const showImages = (images) => {
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2 ';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    gallery.appendChild(div)
+    toggleSpinner(false);
+  })
+
+}
+
+const getImages = (query) => {
+  toggleSpinner(true);
+  fetch(`https://pixabay.com/api/?key=${KEY}&q=${query}&image_type=photo &pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => console.log(err))
+}
+
+let slideIndex = 0;
+const selectItem = (event, img) => {
+  let element = event.target;
+
+
+
+  let item = sliders.indexOf(img);
+  if (item === -1) {
+    element.classList.add('added');
+    sliders.push(img);
+  } else {
+    //alert('Hey, Already added !')
+    //sliders.pop(img);
+    sliders.splice(item, 1)
+    element.classList.remove('added');
+  }
+
+}
+var timer
+const createSlider = () => {
+  // check slider image length
+  if (sliders.length < 2) {
+    alert('Select at least 2 image.')
+    return;
+  }
+  // crate slider previous next area
+  sliderContainer.innerHTML = '';
+  const prevNext = document.createElement('div');
+  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.innerHTML = ` 
+  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  `;
+
+  sliderContainer.appendChild(prevNext)
+  document.querySelector('.main').style.display = 'block';
+  // hide image aria
+  imagesArea.style.display = 'none';
+  let duration = document.getElementById('duration').value;
+  if (duration > 0000) {
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100" onclick=removeSlider("${slide}") id="slider-item"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item);
+
+    })
+  }
+  else {
+    duration = 2000
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100" onclick=removeSlider("${slide}") id="slider-item"
+      src="${slide}"
+      alt="">`;
+      sliderContainer.appendChild(item);
+    })
+    //alert("Error");
+
+    //imagesArea.style.display = 'block';
+  }
+  console.log(duration);
+  changeSlide(0)
+  timer = setInterval(function () {
+    slideIndex++;
+    changeSlide(slideIndex);
+  }, duration);
+}
+
+// change slider index 
+const changeItem = index => {
+  changeSlide(slideIndex += index);
+}
+
+// change slide item
+const changeSlide = (index) => {
+
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
+
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
+
+  items.forEach(item => {
+    item.style.display = "none"
+  })
+
+  items[index].style.display = "block"
+}
+
+search.addEventListener("keypress", function (event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    document.querySelector('.main').style.display = 'none';
+    clearInterval(timer);
+    const search = document.getElementById('search');
+    getImages(search.value)
+    sliders.length = 0;
+  }
+
+
+})
+
+sliderBtn.addEventListener('click', function () {
+  createSlider()
+})
+const toggleSpinner = (show) => {
+  const spinner = document.getElementById("spinner");
+  if (show) {
+    spinner.classList.remove('d-none')
+  }
+  else {
+    spinner.classList.add('d-none')
+  }
+}
+const removeSlider = (slide) => {
+  document.getElementById("slider-item").style.display = 'none'
+  console.log(90);
+  const a = sliders.indexOf(slide);
+  sliders.splice(a, 1)
+}
